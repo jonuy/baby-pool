bac1ca513f55b97418616fd078df5aa62d4ea33c
jon uy <jonxuy@gmail.com>
2014-06-23 01:42:37 -0400

    Added uri support for MongoLab heroku plugin

--- app/models/submission.js
+++ app/models/submission.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose')
     ;
 
-var uri = 'mongodb://localhost/baby_pool';
+// MONGOLAB_URI set by heroku in conjunction with the MongoLab plugin.
+// Otherwise, use the one explicitly set for localhost.
+var uri = process.env.MONGOLAB_URI || 'mongodb://localhost/baby_pool';
 var db = mongoose.createConnection(uri);
 
 var schema = new mongoose.Schema({
