7431830bb8dbb7fa18ca868b4257f0581a8b09e9
jon uy <jonxuy@gmail.com>
2014-06-23 23:11:07 -0400

    Initial pass at populating 'results' view with live data

--- app/results.js
+++ app/results.js
@@ -4,8 +4,66 @@ var model = require('./models/submission')
 module.exports = function(app) {
 
   var render = function(request, response) {
-    // @TODO populate the results view with the submissions
-    response.render('results');
+
+    // Retrieve all documents in the collection
+    model.find({}, function(err, docs) {
+      if (err) {
+        response.send(500);
+        return;
+      }
+
+      var birthdates = []
+          , weights = []
+          , heights = []
+          , hairs = []
+          ;
+
+      // Loop through documents to populate the results to pass to the view
+      docs.forEach(function(val, idx, set) {
+        var name = val.first_name + ' ' + val.last_name;
+
+        // Birthdate results
+        var birthdatesIndex = birthdates.length;
+        birthdates[birthdatesIndex] = {};
+        birthdates[birthdatesIndex].name = name;
+        birthdates[birthdatesIndex].date = val.birthdate.toLocaleDateString();
+        birthdates[birthdatesIndex].time = val.birthdate.toLocaleTimeString();
+
+        // Height results
+        var heightsIndex = heights.length;
+        heights[heightsIndex] = {};
+        heights[heightsIndex].name = name;
+        heights[heightsIndex].height = val.height_inches;
+
+        // Weight results
+        var weightsIndex = weights.length;
+        weights[weightsIndex] = {};
+        weights[weightsIndex].name = name;
+        weights[weightsIndex].weight = val.weight_lbs + ' lbs ' + val.weight_oz + ' oz';
+
+        // Hair results
+        var hairsIndex = hairs.length;
+        hairs[hairsIndex] = {};
+        hairs[hairsIndex].name = name;
+        hairs[hairsIndex].hair = val.hair ? 'YES' : 'NO';
+      });
+
+      // @TODO sort birthdates by date and time
+      // @TODO sort heights in ascending order
+      // @TODO sort weights in ascending order
+      // @TODO sort hair options to YES and NO
+      
+      // Prepare object to pass to the view to parse
+      var results = {
+        birthdates: birthdates,
+        weights: weights,
+        heights: heights,
+        hairs: hairs
+      };
+
+      // Render the 'results' view
+      response.render('results', results);
+    });
   };
 
   /**
--- app/submit.js
+++ app/submit.js
@@ -2,13 +2,16 @@ var model = require('./models/submission')
     ;
 
 module.exports = function(app) {
+  var self = {};
+
   var onFormSubmit = function(request, response) {
+    self.response = response;
+
     // Document created based on request.body payload
     var doc = {
       first_name: request.body.first_name,
       last_name: request.body.last_name,
-      birthdate: new Date(request.body.birthdate),
-      birthdate_time: request.body.birthdate_time,
+      birthdate: new Date(request.body.birthdate + ' ' + request.body.birthdate_time),
       weight_lbs: request.body.weight_lbs,
       weight_oz: request.body.weight_oz,
       height_inches: request.body.height_inches,
@@ -18,9 +21,6 @@ module.exports = function(app) {
     // Add document to the database
     var submission = model.create(doc);
     submission.then(onCreate);
-
-    // Redirect to the results view
-    response.redirect('/results');
   };
 
   var onCreate = function(doc) {
@@ -28,6 +28,12 @@ module.exports = function(app) {
       console.log('Document created successfully.');
       console.log(doc);
     }
+    else {
+      console.log('uh oh...');
+    }
+
+    // Redirect to the results view
+    self.response.redirect('/results');
   };
 
   /**
